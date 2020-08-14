f6380ce0801828e64471cb9986beadb836bc8bb0
VirajKanse <virajv2k@gmail.com>
2020-08-14 09:04:10 +0000

    Fix few paths

--- patch.sh
+++ patch.sh
@@ -4,7 +4,7 @@ echo "Regenerating FaceUnlock..."
 ROOTDIR="$PWD"
 CURRENT_DIR="$ROOTDIR/random-stuff"
 
-cd packages/apps/Settings
+cd $ROOTDIR/packages/apps/Settings
 git am $CURRENT_DIR/settings-Port-face-unlock-feature.patch
 
 cd $ROOTDIR/frameworks/base
@@ -13,7 +13,7 @@ git am $CURRENT_DIR/base-Port-face-unlock-feature.patch
 cd $ROOTDIR/vendor/revengeos
 git am $CURRENT_DIR/vendor-revengeos-Include-faceunlock-config-if-available.patch
 
-cd packages/apps/Settings
+cd $ROOTDIR/packages/apps/Settings
 git am $CURRENT_DIR/settings-Add-moto-faceunlock-string.patch
 
 cd $ROOTDIR/vendor/revengeui
