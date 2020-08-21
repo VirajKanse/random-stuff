7aa18e944216a90726af78caad683e1998bf6251
VirajKanse <virajv2k@gmail.com>
2020-08-21 18:53:40 +0000

    Update new patches

--- patch.sh
+++ patch.sh
@@ -10,14 +10,11 @@ git am $CURRENT_DIR/settings-Port-face-unlock-feature.patch
 cd $ROOTDIR/frameworks/base
 git am $CURRENT_DIR/base-Port-face-unlock-feature.patch
 
-cd $ROOTDIR/vendor/revengeos
-git am $CURRENT_DIR/vendor-revengeos-Include-faceunlock-config-if-available.patch
+cd $ROOTDIR/vendor/carbon
+git am $CURRENT_DIR/vendor-carbon-Include-faceunlock-config-if-available.patch
 
-cd $ROOTDIR/packages/apps/Settings
-git am $CURRENT_DIR/settings-Add-moto-faceunlock-string.patch
-
-cd $ROOTDIR/vendor/revengeui
-git am $CURRENT_DIR/vendor-revengeui-Update-default-wallpaper.patch
+cd $ROOTDIR/frameworks/base
+git am $CURRENT_DIR/BoostFramework-To-Enhance-performance.patch
 
 cd $ROOTDIR
 echo "Done"
